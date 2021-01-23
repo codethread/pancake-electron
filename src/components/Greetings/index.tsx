feb4e8879847cff4b6b804311abcec8b4c8c9d99
Adam Hall <adamhalldesigns@gmail.com>
2021-01-23 18:13:04 +0000

    fix lint to run on src (#2)

--- src/components/Greetings/index.tsx
+++ src/components/Greetings/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-import { Container, Image, Text } from './styles'
-import Button from '../Button'
+import { Container, Image, Text } from './styles';
+import Button from '../Button';
 
 const Greetings: React.FC = () => {
   return (
@@ -10,10 +10,12 @@ const Greetings: React.FC = () => {
         src="https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg"
         alt="ReactJS logo"
       />
-      <Text>An Electron boilerplate including TypeScript, React, Jest and ESLint.</Text>
+      <Text>
+        An Electron boilerplate including TypeScript, React, Jest and ESLint.
+      </Text>
       <Button>I am a button</Button>
     </Container>
-  )
-}
+  );
+};
 
-export default Greetings
+export default Greetings;
