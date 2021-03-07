90cc3dd3458fc53ed252806066abae0af5a09a6d
Adam Hall <adamhalldesigns@gmail.com>
2021-03-07 11:03:31 +0000

    remove lint ignore

--- client/components/Greetings/Greetings.spec.tsx
+++ client/components/Greetings/Greetings.spec.tsx
@@ -8,7 +8,6 @@ test('Greetings should renders', () => {
   const { getByText, getByAltText } = render(<Greetings />);
 
   const greeting = getByText('Welcome to version', { exact: false });
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   expect(greeting).toHaveTextContent(new RegExp(pj.version));
   expect(getByAltText('ReactJS logo')).toBeTruthy();
 });
