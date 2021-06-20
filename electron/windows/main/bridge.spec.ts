3e6dc49a5d7f66df3325c0e3fc13a088735d590d
Adam Hall <adamhalldesigns@gmail.com>
2021-06-20 18:13:15 +0100

    fix test for bridge

--- electron/windows/main/bridge.spec.ts
+++ electron/windows/main/bridge.spec.ts
@@ -6,7 +6,7 @@ describe('bridgeCreator', () => {
   const str = 'string';
 
   interface Test {
-    args: string[];
+    args: any[];
     method: keyof IBridge;
     ipc: keyof IpcRenderer;
   }
@@ -19,10 +19,12 @@ describe('bridgeCreator', () => {
     ${'validateAndStoreGithubToken'} | ${[str]}      | ${'invoke'}
     ${'getCurrentUser'}              | ${[]}         | ${'invoke'}
     ${'loadUserConfig'}              | ${[]}         | ${'invoke'}
+    ${'updateUserConfig'}            | ${[str]}      | ${'invoke'}
   `(
     'bridge.$method sends args as an array to bridge method "$ipc"',
     async ({ method, args, ipc }: Test) => {
       const bridge = bridgeCreator(ipcRenderer);
+      // @ts-expect-error I'm not sure how to type the args here, but the the test validates each method passes an array of arguments straight through to the ipc layer.
       await bridge[method](...args);
       expect(ipcRenderer[ipc]).toHaveBeenCalledWith(method, args);
     }
